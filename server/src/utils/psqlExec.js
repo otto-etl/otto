244bedf586c6795d09dcd2e013b66544f503fbe3
Joe Webster <joseph.scott.webster@gmail.com>
2023-06-28 19:13:24 -0400

    Connecting to the database on consecutive load executions does not result in an error

--- server/src/utils/psqlExec.js
+++ server/src/utils/psqlExec.js
@@ -1,56 +1,46 @@
 import pgPromise from "pg-promise";
 import { getNode } from "./node.js";
 import { updateWorkflowNodes } from "../models/pgService.js";
+const pgp = pgPromise();
+
+let db;
 
 export const connectPSQL = async ({ userName, password, dbName }) => {
-  const pgp2 = pgPromise();
   try {
-    const db = await pgp2(
-      `postgres://${userName}:${password}@localhost:5432/${dbName}`
-    );
-    return db;
+    return pgp(`postgres://${userName}:${password}@localhost:5432/${dbName}`);
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
 export const runPSQLCode = async (workflowObj, nodeObj) => {
-  const prevNodeID = nodeObj.data.prev;
-  const previousNode = getNode(workflowObj, prevNodeID);
-  const { userName, password, dbName, sqlCode } = nodeObj.data;
+  try {
+    const prevNodeID = nodeObj.data.prev;
+    const previousNode = getNode(workflowObj, prevNodeID);
+    const { userName, password, dbName, sqlCode } = nodeObj.data;
 
-  // console.log("prevNodeID", prevNodeID);
-  // console.log("prevNode", previousNode);
-  // console.log("Data attributes", userName, password, dbName);
-  // console.log("SQL code", sqlCode);
+    if (!db) {
+      db = await connectPSQL({ userName, password, dbName });
+      console.log("Connection to the database established.");
+    } else {
+      console.log("Using existing database connection.");
+    }
 
-  // the array of data we need to insert
+    const dataToInsert = previousNode.data.output.data;
+    const regex = /\$\{([^}]+)\}/g;
+    const insertFields = [...sqlCode.matchAll(regex)].map((returnedData) => {
+      return returnedData[1];
+    });
 
-  const isConnectionExists = pgp2.end() === undefined;
-
-  // if (isConnectionExists) {
-  //   console.log("Connection to the database already exists.");
-  // } else {
-
-  const db = await connectPSQL({ userName, password, dbName });
-
-  //   console.log("No existing connection to the database.");
-  // }
-
-  const dataToInsert = previousNode.data.output.data;
-  const regex = /\$\{([^}]+)\}/g;
-  const insertFields = [...sqlCode.matchAll(regex)].map(
-    (returnedData) => returnedData[1]
-  );
-
-  console.log("data to insert", dataToInsert);
-  console.log("insert fields", insertFields);
-  dataToInsert.forEach((obj) => {
-    db.any(sqlCode, matchDataPropWithVarNames(insertFields, obj));
-  });
+    for (const obj of dataToInsert) {
+      await db.any(sqlCode, matchDataPropsWithVarNames(insertFields, obj));
+    }
+  } catch (error) {
+    console.error("Error running PSQL code:", error);
+  }
 };
 
-const matchDataPropWithVarNames = (insertFields, obj) => {
+const matchDataPropsWithVarNames = (insertFields, obj) => {
   const result = {};
   insertFields.forEach((field) => (result[field] = obj[field]));
   return result;
