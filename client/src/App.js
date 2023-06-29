76258e5cf32c8183b2c8a0dcdb8dab2952695e05
Matthew Sisserson <masisserson@gmail.com>
2023-06-29 12:12:10 -0400

    Moved App.js into src

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Workflow from "./components/Workflow";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo">
+      <h3>React Flow demo</h3>
+      <Workflow />
     </div>
   );
-}
+};
 
 export default App;
