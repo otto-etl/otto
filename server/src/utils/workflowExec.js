417425cbbc128c8f7e4559b7451df581f7bcb9ec
Matthew Sisserson <masisserson@gmail.com>
2023-07-07 17:13:17 -0400

    Added memoization so that nodes are not repeatedly executed

--- server/src/utils/workflowExec.js
+++ server/src/utils/workflowExec.js
@@ -7,18 +7,23 @@ import { updateWorkflowError, updateNodes } from "../models/pgService.js";
 import { workflowInputvalidation } from "./workflowInput.js";
 import { throwNDErrorAndUpdateDB } from "./errors.js";
 
+let completedNodes = {};
+
 const executeNode = async (workflowObj, nodeObj) => {
-  console.log("Executing node");
-  console.log(nodeObj);
-  if (nodeObj.type === "extract") {
-    await runAPI(workflowObj, nodeObj);
-  } else if (nodeObj.type === "transform") {
-    await runJSCode(workflowObj, nodeObj);
-  } else if (nodeObj.type === "load") {
-    await runPSQLCode(workflowObj, nodeObj);
-  } else if (nodeObj.type !== "trigger") {
-    const message = `Invalid Node Type: ${nodeObj.type}`;
-    await throwNDErrorAndUpdateDB(workflowObj, nodeObj, message);
+  if (completedNodes[nodeObj.id]) {
+    return;
+  } else {
+    if (nodeObj.type === "extract") {
+      await runAPI(workflowObj, nodeObj);
+    } else if (nodeObj.type === "transform") {
+      await runJSCode(workflowObj, nodeObj);
+    } else if (nodeObj.type === "load") {
+      await runPSQLCode(workflowObj, nodeObj);
+    } else if (nodeObj.type !== "trigger") {
+      const message = `Invalid Node Type: ${nodeObj.type}`;
+      await throwNDErrorAndUpdateDB(workflowObj, nodeObj, message);
+    }
+    completedNodes[nodeObj.id] = true;
   }
 };
 
@@ -26,7 +31,7 @@ const activateNode = async (workflowObj, nodeObj) => {
   const edges = workflowObj.edges.filter((edge) => edge.target === nodeObj.id);
 
   if (edges.length === 0) {
-    await executeNode(workflowObj, nodeObj);
+    return;
   } else {
     const nodes = edges.map((edge) => getNode(workflowObj, edge.source));
     const promises = nodes.map((node) => {
@@ -42,12 +47,22 @@ const activateNode = async (workflowObj, nodeObj) => {
 export const runWorkflow = async (workflowObj) => {
   console.log("running workflow", workflowObj.id);
   await workflowInputvalidation(workflowObj);
-  const finalNodes = workflowObj.nodes.filter((node) => node.type === "load");
+  const finalLoadNodes = workflowObj.nodes.filter(
+    (node) => node.type === "load"
+  );
 
-  for (const node of finalNodes) {
-    await activateNode(workflowObj, node);
-  }
+  const promises = finalLoadNodes.map((node) => {
+    return new Promise((res, rej) => {
+      res(activateNode(workflowObj, node));
+    });
+  });
+  await Promise.all(promises);
 
+  // for (const node of finalLoadNodes) {
+  //   await activateNode(workflowObj, node);
+  // }
+
+  completedNodes = {};
   console.log("workflow completed", workflowObj.id);
   await updateWorkflowError(workflowObj.id, null);
 };
