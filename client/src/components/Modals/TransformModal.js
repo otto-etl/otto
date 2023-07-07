1272f270a232f43e76e0854c3408c77a14923a5f
MASisserson <66091016+MASisserson@users.noreply.github.com>
2023-07-07 14:45:39 -0400

    Routes (#26)
    * Added working Python editor
    
    * Polished component files

--- client/src/components/Modals/TransformModal.js
+++ client/src/components/Modals/TransformModal.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
+import CustomTabPanel from "../CustomTabPanel";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
-
+import { python } from "@codemirror/lang-python";
 
 const TransformModal = ({ nodeObj, handleSubmit, active, handleDelete }) => {
   console.log(nodeObj.data);
   const [name, setName] = useState(nodeObj.data.label);
   const [code, setCode] = useState(nodeObj.data.jsCode);
+  const [tab, setTab] = useState(0);
 
   const handleChange = React.useCallback((value, viewupdate) => {
     setCode(value);
   }, []);
+
+  const handleTabChange = React.useCallback((e, newValue) => {
+    setTab(newValue);
+  }, []);
+
+  const a11yProps = (index) => {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  };
 
   return (
     <Box>
@@ -40,14 +55,34 @@ const TransformModal = ({ nodeObj, handleSubmit, active, handleDelete }) => {
         />
         <br></br>
         <br></br>
-        <CodeMirror
-          readOnly={active ? true : false}
-          value={code}
-          height="200px"
-          extensions={[javascript({ jsx: true })]}
-          onChange={handleChange}
-        />
-		<Button onClick={handleDelete} disabled={active ? true : false}>Delete Node</Button>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={tab}
+            onChange={handleTabChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Javascript" {...a11yProps(0)} />
+            <Tab label="Python" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={tab} index={0}>
+          <CodeMirror
+            readOnly={active ? true : false}
+            value={code}
+            height="200px"
+            extensions={[javascript({ jsx: true })]}
+            onChange={handleChange}
+          />
+        </CustomTabPanel>
+        <CustomTabPanel value={tab} index={1}>
+          <CodeMirror
+            readOnly={active ? true : false}
+            value={code}
+            height="200px"
+            extensions={[python()]}
+            onChange={handleChange}
+          />
+        </CustomTabPanel>
         <br></br>
         <br></br>
         <Button
@@ -57,6 +92,9 @@ const TransformModal = ({ nodeObj, handleSubmit, active, handleDelete }) => {
           disabled={active ? true : false}
         >
           Save and Execute
+        </Button>
+        <Button onClick={handleDelete} disabled={active ? true : false}>
+          Delete Node
         </Button>
       </form>
     </Box>
