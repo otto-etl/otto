687bb3717c3e4e7843fa4ca8a6e9025ae5528eeb
Matthew Sisserson <masisserson@gmail.com>
2023-07-07 16:25:03 -0400

    Global execute workflow can now theoretically handle branching logic

--- server/src/utils/workflowExec.js
+++ server/src/utils/workflowExec.js
@@ -1,4 +1,4 @@
-import { getAllNodesInOrder, getTriggerNode } from "./node.js";
+import { getNode } from "./node.js";
 import { runAPI } from "./apiExec.js";
 import { runJSCode } from "./jsCodeExec.js";
 import { runPSQLCode } from "./psqlExec.js";
@@ -7,24 +7,47 @@ import { updateWorkflowError, updateNodes } from "../models/pgService.js";
 import { workflowInputvalidation } from "./workflowInput.js";
 import { throwNDErrorAndUpdateDB } from "./errors.js";
 
+const executeNode = async (workflowObj, nodeObj) => {
+  console.log("Executing node");
+  console.log(nodeObj);
+  if (nodeObj.type === "extract") {
+    await runAPI(workflowObj, nodeObj);
+  } else if (nodeObj.type === "transform") {
+    await runJSCode(workflowObj, nodeObj);
+  } else if (nodeObj.type === "load") {
+    await runPSQLCode(workflowObj, nodeObj);
+  } else if (nodeObj.type !== "trigger") {
+    const message = `Invalid Node Type: ${nodeObj.type}`;
+    await throwNDErrorAndUpdateDB(workflowObj, nodeObj, message);
+  }
+};
+
+const activateNode = async (workflowObj, nodeObj) => {
+  const edges = workflowObj.edges.filter((edge) => edge.target === nodeObj.id);
+
+  if (edges.length === 0) {
+    await executeNode(workflowObj, nodeObj);
+  } else {
+    const nodes = edges.map((edge) => getNode(workflowObj, edge.source));
+    const promises = nodes.map((node) => {
+      return new Promise((res, rej) => {
+        res(activateNode(workflowObj, node));
+      });
+    });
+    await Promise.all(promises);
+    await executeNode(workflowObj, nodeObj);
+  }
+};
+
 export const runWorkflow = async (workflowObj) => {
   console.log("running workflow", workflowObj.id);
   await workflowInputvalidation(workflowObj);
+  const finalNodes = workflowObj.nodes.filter((node) => node.type === "load");
 
-  const nodesToExecute = getAllNodesInOrder(workflowObj);
-
-  for (const nodeObj of nodesToExecute) {
-    if (nodeObj.type === "extract") {
-      await runAPI(workflowObj, nodeObj);
-    } else if (nodeObj.type === "transform") {
-      await runJSCode(workflowObj, nodeObj);
-    } else if (nodeObj.type === "load") {
-      await runPSQLCode(workflowObj, nodeObj);
-    } else if (nodeObj.type !== "trigger") {
-      const message = `Invalid Node Type: ${nodeObj.type}`;
-      await throwNDErrorAndUpdateDB(workflowObj, nodeObj, message);
-    }
+  for (const node of finalNodes) {
+    await activateNode(workflowObj, node);
   }
+
   console.log("workflow completed", workflowObj.id);
   await updateWorkflowError(workflowObj.id, null);
 };
