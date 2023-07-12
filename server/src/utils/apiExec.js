5aaffe893ae21c438354e9839e0733a5b4956a07
Jing Zhu <jing.harrell@gmail.com>
2023-07-12 13:49:52 -0500

    OAuth2 for client credentials flow works, need to work on token expiration
    Co-authored-by: Joe Webster <joseph.scott.webster@gmail.com>

--- server/src/utils/apiExec.js
+++ server/src/utils/apiExec.js
@@ -2,30 +2,14 @@ import { updateNodes } from "../models/workflowsService.js";
 import { throwNDErrorAndUpdateDB, throwEXErrorAndUpdateDB } from "./errors.js";
 import { nodeInputvalidation } from "./nodeInput.js";
 import axios from "axios";
-import oauth from "axios-oauth-client";
 
 export const runAPI = async (workflowObj, nodeObj) => {
   await nodeInputvalidation(workflowObj, nodeObj);
-  const input = {
-    url: nodeObj.data.url,
-    method: nodeObj.data.httpVerb,
-    data: nodeObj.data.bodyChecked ? nodeObj.data.jsonBody : {},
-    headers: nodeObj.data.headerChecked ? nodeObj.data.header : {},
-  };
-  const oAuth = {
-    oAuthChecked: nodeObj.data.oAuthChecked,
-    accessTokenURL: nodeObj.data.accessTokenURL,
-    clientID: nodeObj.data.clientID,
-    clientSecret: nodeObj.data.clientSecret,
-    scope: nodeObj.data.scope,
-  };
-  console.log(input);
-  console.log(oAuth);
   let data;
-
   try {
     //change to oAuthAndSend
-    data = await sendAPI(input);
+    data = await oAuthAndSend(nodeObj);
+    console.log("oAuthAndSned result", data);
   } catch (e) {
     const status = e.toJSON().status;
     const code = e.toJSON().code;
@@ -60,7 +44,6 @@ export const runAPI = async (workflowObj, nodeObj) => {
 };
 
 const sendAPI = async ({ method, url, data, headers }) => {
-  console.log({ method, url, data, headers });
   const response = await axios({ method, url, data, headers });
   return response.data;
 };
@@ -72,35 +55,59 @@ const getAccessToken = async (
   clientSecret,
   scope
 ) => {
-  const getClientCredentials = oauth.clientCredentials(
-    axios.create(),
+  const headers = {
+    Authorization:
+      "Basic " +
+      new Buffer.from(clientID + ":" + clientSecret).toString("base64"),
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  const data = { grant_type: "client_credentials", scope: scope };
+  const response = await axios({
+    method: "POST",
+    url: accessTokenURL,
+    data,
+    headers,
+  });
+  nodeObj.data["token"] = response.data;
+};
+
+const oAuthAndSend = async (nodeObj) => {
+  let {
+    httpVerb,
+    url,
+    header,
+    jsonBody,
     accessTokenURL,
     clientID,
-    clientSecret
-  );
-  const auth = await getClientCredentials(scope);
-  // auth {access_token = ...., refresh_token =..., expires =....}
-  nodeObj.data.token = auth;
-};
+    clientSecret,
+    scope,
+    oAuthChecked,
+  } = nodeObj.data;
 
-const refreshToken = () => {
-  //if there is a refresh token, use the refreshToken to get new access token
-  //otherwise call the get AccessToken function
-};
-
-const oAuthAndSend = (nodeObj, input, oAuth) => {
-  const { method, url, data, headers } = input;
-  const { accessTokenURL, clientID, clientSecret, scope, oAuthChecked } = oAuth;
   //check if nodeObj.data has access token
   if (oAuthChecked) {
-    if (nodeObj.data.token) {
-      getAccessToken(nodeObj, accessTokenURL, clientID, clientSecret, scope);
+    if (!nodeObj.data.token) {
+      await getAccessToken(
+        nodeObj,
+        accessTokenURL,
+        clientID,
+        clientSecret,
+        scope
+      );
     }
-    headers["authorization"] = `bearer ${nodeObj.auth.access_token}`;
+    header["Authorization"] = "Bearer " + nodeObj.data.token.access_token;
+    let data;
+    if (Object.keys(jsonBody).length === 0) {
+      data = undefined;
+    } else {
+      data = jsonBody;
+    }
+    const res = await sendAPI({
+      method: httpVerb,
+      url,
+      headers: header,
+      data,
+    });
+    return res;
   }
-
-  sendAPI({ method, url, data, headers });
-  //yes: add to the headers and then sendAPI
-  //no: call getAccessToken and then add token to headers and send API
-  //catch error that says token expires, call refresh token, then add token to headers and send API
 };
